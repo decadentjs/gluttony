c545d7d1b6628b875076273604ec4f1317301241
claudiopro <claudio.procida@gmail.com>
2015-11-10 20:02:31 +0100

    Made to work with rimraf and npm.commands

---
+++ README.markdown
@@ -0,0 +1,21 @@
+# gluttony
+
+Nukes the project's `node_modules` directory and reinstalls latest dependencies from `package.json`.
+
+# Usage
+
+Install
+
+```bash
+npm install -g gluttony
+```
+
+Use to reinstall dependencies of a project
+
+```bash
+gluttony
+```
+
+# License
+
+MIT
--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 'use strict';
 
 const pjson = require('pjson'),
+       path = require('path'),
+         fs = require('fs'),
+     rimraf = require('rimraf'),
+        npm = require('npm'),
        keys = Object.keys || require('object-keys');
 
-console.log(__dirname);
-console.log(keys(pjson.dependencies));
+const NODE_MODULES = path.resolve('./node_modules'),
+              DEPS = pjson.dependencies && keys(pjson.dependencies),
+          DEV_DEPS = pjson.devDependencies && keys(pjson.devDependencies);
+
+function reinstall() {
+  npm.load({}, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    [DEPS, DEV_DEPS].forEach(function(deps){
+      if (deps) {
+        npm.commands.install(deps, function (err, data) {
+          if (err) {
+            return;
+          }
+          console.log('Yay! Dependencies reinstalled from scratch.');
+        });
+      }
+    });
+  });
+}
+
+fs.stat(NODE_MODULES, function(err, stats) {
+  if (err) {
+    switch (err.code) {
+      case 'ENOENT':
+        // node_modules doesn't exist: just reinstall
+        reinstall();
+        break;
+    }
+    return;
+  }
+  // call rm -rf on the NODE_MODULES dir
+  rimraf(NODE_MODULES, function(err) {
+    if (err) {
+      // switch (err.code) {
+      //   case 'ENOENT':
+      //     console.log('');
+      //     break;
+      // }
+      console.log(err);
+      return;
+    }
+    reinstall();
+  });
+});
